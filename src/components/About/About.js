1011d685de48c853ed53eecd92d63d6f2ca8bcc3
skusuma-ub <skusuma@my.bridgeport.edu>
2024-02-06 15:46:38 -0500

    Skills chnages commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from "react";
 import pic from "../../assets/mypic.png";
 import './About.css'
 
 const About = () => {
 
-    /*const [classes, setClasses] = useState('');
-
-    useEffect(() => {
-        setClasses('visibleabout');
-    },[])*/
 
     return (
         <section  id="about">
             <div className="about">
                 <p>I'm <span>Suresh Krishna Kusuma</span> <br />
-                    Full Stack Developer, code enthusisat
+                    Software Engineer | Full Stack Developer | Java, AWS, Kubernetes, Apache Kafka Certified
                 </p>
                 <br />
                 <a href="#contact">Contact me</a>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from "react";
 import './Skills.css';
 import javascript from "../../assets/javascript_logo.png";
 import angular_logo from "../../assets/angular_logo.png";
-import aws_logo from "../../assets/aws_logo.png";
+import aws_logo from "../../assets/aws_logo.jpg";
 import docker_logo from "../../assets/docker_logo.png";
-import kubernetes_logo from "../../assets/kubernetes_logo.jpg";
+import kubernetes_logo from "../../assets/kubernetes_logo.png";
 import java_logo from "../../assets/java_logo.png";
 import mongodb_logo from "../../assets/mongodb_logo.png";
+import react_logo from "../../assets/react_logo.png";
+import html_logo from "../../assets/html_logo.jpg";
+import jenkins_logo from "../../assets/Jenkins_logo.png";
+import spring_logo from "../../assets/spring_logo.jpg";
+import MySQL_logo from "../../assets/MySQL_logo.jpg";
+
 
 
 const Skills = () => {
 
-    const [extraClasses, setExtraClasses] = useState('');
 
     return(
         <section id='skills'>
             <h2>Skills</h2>
-            <div className={extraClasses + "skills-container"}>
+            <div className={"skills-container"}>
+                <div className="skill-box"><img src={html_logo} alt =""/><br/><span>HTML & CSS</span></div>
                 <div className="skill-box"><img src={javascript} alt =""/><br/><span>Javascript</span></div>
                 <div className="skill-box"><img src={angular_logo} alt =""/><br/><span>Angular</span></div>
-                <div className="skill-box"><img src={docker_logo} alt =""/><br/><span>Docker</span></div>
-                <div className="skill-box"><img src={kubernetes_logo} alt =""/><br/><span>Kubernetes</span></div>
+                <div className="skill-box"><img src={react_logo} alt =""/><br/><span>React</span></div>
                 <div className="skill-box"><img src={java_logo} alt =""/><br/><span>Java</span></div>
-                <div className="skill-box"><img src={javascript} alt =""/><br/><span>Javascript</span></div>
-                <div className="skill-box"><img src={javascript} alt =""/><br/><span>Javascript</span></div>
-                <div className="skill-box"><img src={aws_logo} alt =""/><br/><span>AWS</span></div>
+                <div className="skill-box"><img src={spring_logo} alt =""/><br/><span>Spring</span></div>
+                <div className="skill-box"><img src={MySQL_logo} alt =""/><br/><span>MySQL</span></div>
                 <div className="skill-box"><img src={mongodb_logo} alt =""/><br/><span>Mongo DB</span></div>
+                <div className="skill-box"><img src={aws_logo} alt =""/><br/><span>AWS</span></div>
+                <div className="skill-box"><img src={jenkins_logo} alt =""/><br/><span>Jenkins</span></div>
+                <div className="skill-box"><img src={kubernetes_logo} alt =""/><br/><span>Kubernetes</span></div>
+                <div className="skill-box"><img src={docker_logo} alt =""/><br/><span>Docker</span></div>
             </div>
         </section>
     )
